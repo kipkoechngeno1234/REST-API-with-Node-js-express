c635341fbf1be9b510ced5fb85abd779ed088767
Finch Kyle <aron.ngenok@gmail.com>
2026-02-23 16:27:42 +0300

    How get () - HHTP method works

--- indexapp..js
+++ indexapp..js
@@ -6,3 +6,8 @@ app.get()
 app.put()
 app.post()
 app.delete()
+
+  // GET HTTP Method 
+app.get('/', (req, res) => {
+    res.send('The wolrd is a better place with REST-API, NodeJS + ExpressJS');
+});
