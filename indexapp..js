fd44e4ddc2dc6a6f9d4ebb75c11056cb6cf23f1b
Finch Kyle <aron.ngenok@gmail.com>
2026-02-26 18:32:59 +0300

    Greate POST HTTP Method and put it in

--- indexapp..js
+++ indexapp..js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
 
-//HTPP Methods
-app.get()
-app.put()
-app.post()
-app.delete()
+//HTPP Method
+
+const courses = [
+  { id: 1, name: 'Web Development' },
+  { id: 2, name: 'Full Stack Development' },
+  { id: 3, name: 'Backend Development' }
+];
 
   // GET HTTP Method 
 app.get('/', (req, res) => {
     res.send('The wolrd is a better place with REST-API, NodeJS + ExpressJS');
-});
+}); 
+
+  //POST HTTP Method
+app.post('/api/courses', (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
+}); 
