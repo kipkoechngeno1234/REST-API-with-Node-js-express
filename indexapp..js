a6cec559f0c2d654775c60af12f338681c5937bc
Finch Kyle <aron.ngenok@gmail.com>
2026-03-02 10:47:35 +0300

    Input Validation using joi and express.json() middleware

--- indexapp..js
+++ indexapp..js
@@ -4,10 +4,11 @@ const Joi = require('joi');
 const express = require('express');
  // Create an instance of the Express application.
 const app = express();
- 
+  // Use the express.json() middleware to parse incoming JSON requests and make the data available in req.body.
+app.use(express.json());
+
 
 //HTPP Method
-
 const courses = [
   { id: 1, name: 'Web Development' },
   { id: 2, name: 'Full Stack Development' },
@@ -16,26 +17,33 @@ const courses = [
 
   // GET HTTP Method to return all courses
 app.get('/api/courses', (req, res) => {
+  res.send(courses);
+});
+
+   // GET HTTP Method with ID
+app.get('/api/courses/:id', (req, res) => {
+   // Find the course with the given ID in the courses array using the find() method. If a course with the specified ID is found, it will be returned; otherwise, undefined will be returned.
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+
+  if (!course) res.status(404).send('The course with the given ID was not found.');
+  res.send(course);
+});
+
+
+
+  //HTTP POST Method
+app.post('/api/courses', (req, res) => {
   // Define a Joi schema to validate the input data for creating a new course. The schema specifies that the name property must be a string with a minimum length of 3 characters and is required.
   const schema = {
     name: Joi.string().min(3).required()
   };
    // Validate the incoming request data against the defined schema using Joi.validate(). If the validation fails, an error object will be returned, which can be used to send an appropriate response to the client.
   const result = Joi.validate(req.body, schema);
-  res.send(courses);
-});
-
-   // GET HTTP Method with ID
-app.get('/api/courses/:id', (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id));
-
-  // Return 404 if the course with the given ID was not found(object not found).
-  if (!course) res.status(404).send('The course with the given ID was not found.');
+  // Return 400 if the course with the given ID was not found(object not found).
+  if (result.error) { res.status(400).send(result.error.details[0].message);
   res.send(course);
-});
+}
 
-  //HTTP POST Method
-app.post('/api/courses', (req, res) => {
   const course = {
     id: courses.length + 1,
     name: req.body.name
@@ -45,3 +53,5 @@ app.post('/api/courses', (req, res) => {
     // Return the newly created course to the client.
   res.send(course);
 }); 
+
+const port = process.env.PORT || 3000;
